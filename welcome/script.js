171014bfdc8208d65de8755925bd52afc55b6e93
harfore <remiharfoush@gmail.com>
2024-03-17 00:57:42 +0100

    16/03 ok

--- welcome/script.js
+++ welcome/script.js
@@ -4,9 +4,14 @@ const activateParisFrance = document.getElementById('activateParisFrance');
 const activateSingapore = document.getElementById('activateSingapore');
 const activateTokyoJapan = document.getElementById('activateTokyoJapan');
 const activateAmsterdam = document.getElementById('activateAmsterdam');
-const presentLosAngeles = document.getElementById('los_angeles');
+const presentLosAngeles = document.getElementById('presentLosAngeles');
+const presentRioDeJaneiro = document.getElementById('presentRioDeJaneiro');
+const presentParisFrance = document.getElementById('presentParisFrance');
+const presentSingapore = document.getElementById('presentSingapore');
+const presentTokyoJapan = document.getElementById('presentTokyoJapan');
+const presentAmsterdam = document.getElementById('presentAmsterdam')
 
-const presentCities = [activateLosAngeles, activateRioDeJaneiro, activateParisFrance, activateSingapore, activateTokyoJapan, activateAmsterdam];
+const activateCities = [activateLosAngeles, activateRioDeJaneiro, activateParisFrance, activateSingapore, activateTokyoJapan, activateAmsterdam];
 
 const fetchEvents = async (dmaId) => {
 
@@ -24,6 +29,7 @@ const fetchEvents = async (dmaId) => {
         // Construct HTML content for events and attractions
         let htmlContent = '';
         htmlContent += `<div class="event_container">`
+        // htmlContent += `<h2>${_embedded.events[0]._embedded.venues[0].city.name},${_embedded.events[0]._embedded.venues[0].state.name},${_embedded.events[0]._embedded.venues[0].country.countryCode}</h2>`
         events.slice(0, 10).forEach(event => {
             // htmlContent += `<p>${events.dates.start.localDate}</p>`
             const attractions = event._embedded?.attractions || [];
@@ -35,6 +41,7 @@ const fetchEvents = async (dmaId) => {
             // const artistHomepageLink = attraction.externalLinks.homepage;
             attractions.forEach(attraction => {
                 htmlContent += `<div class="event_item">`
+                htmlContent += `<a href="/show/show.html">`
                 htmlContent += `<h2>${attraction.name}</h2>`;
                 console.log(attraction.name)
                 // htmlContent += `<p>Date: ${dates.start.localDate}</p>`;
@@ -42,15 +49,20 @@ const fetchEvents = async (dmaId) => {
                 htmlContent += `<p>${attraction.classifications?.genre?.name || ''}</p>`;
                 htmlContent += `<p>${attraction.classifications?.subGenre?.name || ''}</p>`;
                 htmlContent += `<p>${attraction.classifications?.subGenre?.name || ''}</p>`;
+                htmlContent += `</a>`
                 htmlContent += `</div>`
 
             })
         });
         htmlContent += `</div>`;
-        presentLosAngeles.innerHTML = htmlContent;
+        let town = '';
+        if (dmaId === 324) {
+            town = presentLosAngeles
+        }
+        town.innerHTML = htmlContent;
         const displayEventsOnResize = () => {
             if (window.matchMedia("(max-width: 768px)").matches) {
-                presentLosAngeles.innerHTML = htmlContent; // Re-render events
+                town.innerHTML = htmlContent; // Re-render events
             }
         };
 
@@ -60,10 +72,12 @@ const fetchEvents = async (dmaId) => {
 
     } catch (err) {
         console.error(err);
-        presentLosAngeles.innerHTML = `<p>${err}</p>`;
+        town.innerHTML = `<p>${err}</p>`;
     };
 };
 
 fetchEvents(324);
 
 // boutons villes
+// a - text decoration: none
+// villes console.log cities.js
