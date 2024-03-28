5c3e8fd71b39b266dadcadbaeabc17efd5bea3a6
harfore <remiharfoush@gmail.com>
2024-03-28 14:52:52 +0100

    attempt #1 at fixing event link listener, adding variable to show page

--- show/show.js
+++ show/show.js
@@ -12,36 +12,89 @@ const displayEvent = async () => {
 
         const eventIndex = sessionStorage.getItem("eventIndex");
         const pathToEvent = data._embedded?.events[eventIndex];
-
+        const eventImages = pathToEvent?.images[1]?.url;
         const eventHeadline = sessionStorage.getItem("clickedArtist");
         const eventDate = sessionStorage.getItem("eventDate");
         const eventVenue = sessionStorage.getItem("eventVenue");
-
-        const eventImages = pathToEvent.images[1].url;
-        const bookingLink = pathToEvent.url;
+        const bookingLink = pathToEvent?.url;
+        const genre = pathToEvent?.classifications[1]?.genre.name;
+        const subGenre = pathToEvent?.classifications[2]?.subGenre.name;
+        // const presaleStartDate = pathToEvent?.sales?.presales[0]?.startDateTime;
+        const generalSaleStartDate = pathToEvent?.sales?.public.startDateTime;
+        const minPrice = pathToEvent?.classifications[0].priceRanges[0].min;
+        const maxPrice = pathToEvent?.classifications[0].priceRanges[0].max;
+        const ticketLimit = pathToEvent?.accessibility?.ticketLimit;
+        const eventPromoter = pathToEvent.promoter.name;
 
         const showPresentation = document.getElementById('eventChoice');
 
 
         let showEventHtmlContent = '';
 
-        showEventHtmlContent += `<img class="eventImage" src="${eventImages}"/>`;
-        showEventHtmlContent += `<h2>${eventHeadline}</h2>`;
-        showEventHtmlContent += `<h3>${eventDate}</h3>`;
-        showEventHtmlContent += `<h3>${eventVenue}</h3>`;
+        if (eventImages) {
+            showEventHtmlContent += `<img class="eventImage" src="${eventImages}"/>`
+        } else if (eventImages === 'Undefined') {
+            eventImages = '';
+        }
+        if (eventHeadline) {
+            showEventHtmlContent += `<h3>${eventHeadline}</h3>`
+        } else if (eventHeadline === 'Undefined') {
+            eventHeadline = '';
+        }
+        if (eventDate) {
+            showEventHtmlContent += `<h4>${eventDate}</h4>`;
+        } else if (eventDate === 'Undefined') {
+            eventDate = '';
+        }
+        if (eventVenue) {
+            showEventHtmlContent += `<h3>${eventVenue}</h3>`
+        } else if (eventVenue === 'Undefined') {
+            eventVenue = '';
+        }
+        if (bookingLink) {
+            showEventHtmlContent += `<p>${bookingLink}</p>`
+        } else if (bookingLink === 'Undefined') {
+            bookingLink = '';
+        }
+        if (genre && subGenre) {
+            showEventHtmlContent += `<p>${genre}, ${subGenre}</p>`
+        } else if (genre === 'Undefined' || subGenre === "Undefined") {
+            genre = '';
+            subGenre = '';
+        }
+        if (generalSaleStartDate) {
+            let newGeneralSaleStartDate = generalSaleStartDate.replace(/T|Z/g, " ")
+            showEventHtmlContent += `<p>Tickets sale starts on ${newGeneralSaleStartDate}</p>`;
+        } else if (generalSaleStartDate === "undefined") {
+            generalSaleStartDate = '';
+        }
+        if (minPrice && maxPrice) {
+            showEventHtmlContent += `Prices range from ${minPrice}$ to ${maxPrice}$.`
+        }
+        if (eventPromoter) {
+            showEventHtmlContent += `<p>Promoter: ${eventPromoter}</p>`
+        } else if (eventPromoter === 'Undefined') {
+            eventPromoter = '';
+        }
 
-        const bookingLinkButton = "<button class='button'>SHOW BOOKING LINK</button>";
+        const bookingLinkButton = document.createElement('button');
+        bookingLinkButton.className = 'button';
+        bookingLinkButton.textContent = 'SHOW BOOKING LINK!';
+        // showEventHtmlContent += bookingLinkButton.outerHTML;
+
+        // bookingLinkButton.addEventListener('click', function () {
+        //     bookingLinkButton.style.display = 'none';
+        //     console.log("hiding link button")
+        //     showEventHtmlContent += `<p><a href="${bookingLink}" target="_blank">${bookingLink}</a></p>`;
+        //     console.log("displaying link");
+        // });
+
+
+
         showPresentation.innerHTML = showEventHtmlContent;
 
-        // showEventHtmlContent += `${bookingLinkButton}`
-        // bookingLinkButton.addEventListener('click', function () {
-        //     showEventHtmlContent += `<p>${bookingLink}</p>`;
-        // });
-        // // FIX BOOKING LINK
-        // showPresentation.innerHTML = showEventHtmlContent;
-
     } catch (err) {
-        console.error(err);
+        console.error("An error has occured while fetching data:" + err);
     };
 };
 
